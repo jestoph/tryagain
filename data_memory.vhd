f0af4404fd7b0e6adb707891fc08cfe32bcc3961
yicupp <38128780+yicupp@users.noreply.github.com>
2018-04-24 18:32:43 +1000

    initial commit of assembly code

--- data_memory.vhd
+++ data_memory.vhd
@@ -36,7 +36,7 @@ end data_memory;
 
 architecture behavioral of data_memory is
 
-type mem_array is array(0 to 15) of std_logic_vector(15 downto 0);
+type mem_array is array(0 to 4096) of std_logic_vector(15 downto 0);
 signal sig_data_mem : mem_array;
 --signal sig_data_mem_tmp : mem_array;
 --signal sig_var_addr_b : std_logic_vector (11 downto 0);
@@ -65,22 +65,60 @@ begin
         
         if (reset = '1') then
             -- initial values of the data memory : reset to zero 
-            var_data_mem(0)  := X"0005";
-            var_data_mem(1)  := X"0008";
-            var_data_mem(2)  := X"0000";
-            var_data_mem(3)  := X"0000";
-            var_data_mem(4)  := X"0000";
-            var_data_mem(5)  := X"0000";
-            var_data_mem(6)  := X"0000";
-            var_data_mem(7)  := X"0000";
-            var_data_mem(8)  := X"0000";
-            var_data_mem(9)  := X"0000";
-            var_data_mem(10) := X"0000";
-            var_data_mem(11) := X"0000";
-            var_data_mem(12) := X"0000";
-            var_data_mem(13) := X"0000";
-            var_data_mem(14) := X"0000";
-            var_data_mem(15) := X"0000";
+--            var_data_mem(0)  := X"0005";
+--            var_data_mem(1)  := X"0008";
+--            var_data_mem(2)  := X"0000";
+--            var_data_mem(3)  := X"0000";
+--            var_data_mem(4)  := X"0000";
+--            var_data_mem(5)  := X"0000";
+--            var_data_mem(6)  := X"0000";
+--            var_data_mem(7)  := X"0000";
+--            var_data_mem(8)  := X"0000";
+--            var_data_mem(9)  := X"0000";
+--            var_data_mem(10) := X"0000";
+--            var_data_mem(11) := X"0000";
+--            var_data_mem(12) := X"0000";
+--            var_data_mem(13) := X"0000";
+--            var_data_mem(14) := X"0000";
+--            var_data_mem(15) := X"0000";
+
+            -- initial values of the data memory : reset to zero 
+            var_data_mem(0) := X"000a"; -- START_OF_ARRAY Initial_Pointers[]RPKRPK
+            var_data_mem(1) := X"0014"; -- random_tableKrandom_tableK
+            var_data_mem(2) := X"0026"; -- InputStringInputString
+            var_data_mem(3) := X"0032"; -- OutputStringOutputString
+            var_data_mem(4) := X"0000"; -- END_OF_ARRAY 
+            var_data_mem(5) := X"1234"; -- START_OF_ARRAY Key[]
+            var_data_mem(6) := X"5678"; -- 
+            var_data_mem(7) := X"9abc"; -- 
+            var_data_mem(8) := X"def0"; -- 
+            var_data_mem(9) := X"0000"; -- END_OF_ARRAY 
+            var_data_mem(10) := X"1190"; -- START_OF_ARRAY random_table[]
+            var_data_mem(11) := X"52c8"; -- 
+            var_data_mem(12) := X"b7ce"; -- 
+            var_data_mem(13) := X"d431"; -- 
+            var_data_mem(14) := X"d3cb"; -- 
+            var_data_mem(15) := X"f1b5"; -- 
+            var_data_mem(16) := X"73ab"; -- 
+            var_data_mem(17) := X"bf62"; -- 
+            var_data_mem(18) := X"0000"; -- END_OF_ARRAY 
+            var_data_mem(19) := X"4849"; -- START_OF_ARRAY  InputArray[]
+            var_data_mem(20) := X"4652"; -- 
+            var_data_mem(21) := X"4945"; -- 
+            var_data_mem(22) := X"4e44"; -- 
+            var_data_mem(23) := X"4f00";
+            var_data_mem(24) := X"0000"; -- START_OF_ARRAY  OuputArray
+            var_data_mem(25) := X"0000"; -- 
+            var_data_mem(26) := X"0000"; -- 
+            var_data_mem(27) := X"0000"; -- 
+            var_data_mem(28) := X"0000"; -- 
+            var_data_mem(29) := X"0000"; -- 
+            var_data_mem(30) := X"0000"; -- 
+            var_data_mem(31) := X"0000"; -- 
+            var_data_mem(32) := X"0000"; -- 
+            var_data_mem(33) := X"0000"; -- 
+            var_data_mem(34) := X"0000"; -- END_OF_ARRAY 
+            var_data_mem(35) := X"0000";
 
         elsif (falling_edge(clk) and write_enable = '1') then
             -- memory writes on the falling clock edge 
